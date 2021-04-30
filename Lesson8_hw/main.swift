64602ce01fbdcfd4e6e0b7287041a24695257fed
Vladimir-dev-start <ova.pb@icloud.com>
2021-04-30 06:02:19 +0300

    дз

--- Lesson8_hw/main.swift
+++ Lesson8_hw/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+//1. Задания сданы.
+//2. Вопрос:
 
+protocol HumanProtocol {
+    var height: Int { get set }
+    var weight: Int { get }
+}
+
+class Man: HumanProtocol {
+    
+    var height: Int = 187
+    var weight: Int = 80
+    }
+
+var somebody = Man()
+
+somebody.weight = 87
+
+print(somebody.weight)
+
+/* В данном примере в протоколе у свойства weight установлен { get }, но xcode позволяет проставить ему var (не только let) и поменять его значение. Как это работает? Заранее спасибо! */
